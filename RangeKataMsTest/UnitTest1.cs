bbfe9e4a1dd2b4893b66c7c19b6d39de84ca8b83
Alex Jose Bujosa Cruz <bujosa2014@outlook.com>
2022-06-15 22:31:56 -0400

    Add EndPointsTest

--- RangeKataMsTest/UnitTest1.cs
+++ RangeKataMsTest/UnitTest1.cs
@@ -6,8 +6,32 @@ namespace RangeKataMsTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EndPointTest1()
         {
+            int[] value = { 2, 6 };
+            string Result = RangeKata.Program.endPoints(RangeKata.Program.SymbolOpen.CorcheteAbierto,RangeKata.Program.SymbolClose.ParentesisCerrado, value );
+            Assert.AreEqual("[2,6) endPoints = {2,5}", Result);
+        }
+        [TestMethod]
+        public void EndPointTest2()
+        {
+            int[] value = { 2, 6 };
+            string Result = RangeKata.Program.endPoints(RangeKata.Program.SymbolOpen.CorcheteAbierto, RangeKata.Program.SymbolClose.CorcheteCerrado, value);
+            Assert.AreEqual("[2,6) endPoints = {2,5}", Result);
+        }
+        [TestMethod]
+        public void EndPointTest3()
+        {
+            int[] value = { 2, 6 };
+            string Result = RangeKata.Program.endPoints(RangeKata.Program.SymbolOpen.ParentesisAbierto, RangeKata.Program.SymbolClose.ParentesisCerrado, value);
+            Assert.AreEqual("[2,6) endPoints = {2,5}", Result);
+        }
+        [TestMethod]
+        public void EndPointTest4()
+        {
+            int[] value = { 2, 6 };
+            string Result = RangeKata.Program.endPoints(RangeKata.Program.SymbolOpen.ParentesisAbierto, RangeKata.Program.SymbolClose.CorcheteCerrado, value);
+            Assert.AreEqual("[2,6) endPoints = {2,5}", Result);
         }
     }
 }
